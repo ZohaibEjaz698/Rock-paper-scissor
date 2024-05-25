2df8af009483ecf0aa499d6c8fc7485e24fc6957
ZohaibEjaz698 <154717781+ZohaibEjaz698@users.noreply.github.com>
2024-05-25 13:00:00 +0500

    Update script.js
    added functionality to reset button

--- script.js
+++ script.js
@@ -3,53 +3,65 @@ let compScore = 0;
 
 let choices = document.querySelectorAll(".choice");
 let msg = document.querySelector("#msg");
-let uscore = document.querySelector("#user");
-let cscore = document.querySelector("#comp");
-const genCompchoice = () =>{
-      let options = ["rock","paper","scissor"];
-      let idx = Math.floor(Math.random() *3);
-     return options[idx];
+let user = document.querySelector("#user");
+let comp = document.querySelector("#comp");
+let btn = document.querySelector("#reset");
+
+const genComp = () => {
+    const options = ["rock","paper","scissor"];
+    const idx = Math.floor(Math.random() *3);
+    return options[idx];
 }
 const drawGame = () =>{
-     msg.innerText = "Its a Draw"
-     msg.style.backgroundColor = "#24252a"
+    msg.innerText = "Its a draw";
+    msg.style.backgroundColor = "#24252a";
 }
+
 const showWinner = (userWin,userChoice,compChoice) =>{
     if(userWin){
         userScore++
-        uscore.innerText = userScore;
+        user.innerText = userScore;
         msg.innerText = `Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green"
     }else{
         compScore++;
-        cscore.innerText = compScore;
+        comp.innerText = compScore;
         msg.innerText = `Your ${userChoice} loses to ${compChoice}`;
         msg.style.backgroundColor = "red"
     }
 }
 
-const playGame = (userChoice) =>{
-     console.log(userChoice);
-    const compChoice = genCompchoice();
-    console.log(compChoice);
-    if(userChoice === compChoice){
-        drawGame();
-    }else{
-        let userWin = true;
-        if(userChoice === "rock"){
-            userWin = compChoice === "paper"? false : true;
-        }else if(userChoice === "paper"){
-            userWin = compChoice === "scissor"? false : true;
-        }else{
-            userWin = compChoice === "rock"? false : true;
-        }
-        showWinner(userWin,userChoice,compChoice);
+const playGame = (userChoice) => {
+   console.log(userChoice);
+   const compChoice = genComp();
+   console.log(compChoice);
+   if(userChoice === compChoice){
+    drawGame();
+   }else{
+    let userWin = true;
+    if(userChoice === "rock"){
+        userWin = compChoice === "paper"? false : true;
+    }else if(userChoice === "paper"){
+        userWin = compChoice === "scissor"? false : true;
+    }else {
+        userWin = compChoice === "rock"? false : true;
     }
+    showWinner(userWin,userChoice,compChoice);
+   }
 }
 
-choices.forEach((choice) =>{
-  choice.addEventListener("click",() =>{
-    const userChoice = choice.getAttribute("id");
-    playGame(userChoice);
-  })
+choices.forEach((choice) => {
+    choice.addEventListener("click",() => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    })
 })
+const resetGame = () => {
+    userScore = 0;
+    compScore = 0;
+    user.innerText = 0;
+    comp.innerText = 0;
+    msg.innerText = "Play Your Turn";
+    msg.style.backgroundColor = "#24252a";
+}
+btn.addEventListener("click",resetGame);
